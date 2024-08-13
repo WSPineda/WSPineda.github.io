9997f8d8dd44bc3e6913439b7e80e1d92cfe9598
Wendel Pineda <wendelpineda@mail.weber.edu>
2024-08-13 02:51:59 -0600

    Final descriptions

--- src/components/project-details.js
+++ src/components/project-details.js
@@ -12,7 +12,7 @@ function ProjectDetails() {
       <Link to="/portfolio" className="btn btn-outline-primary mb-4">Go Back</Link>
       
       <div className="card shadow-sm mb-5">
-        <div className="card-body">
+        <div className="card-body text-center">
           <h2 className="card-title">{project.title} Overview</h2>
           <img src={project.image} alt={project.title} className="img-fluid rounded mb-4 detail-image" />
           <p className="card-text">{project.description}</p>
@@ -23,25 +23,25 @@ function ProjectDetails() {
       <div className="row">
         <div className="col-lg-12">
           <div className="card mb-4 shadow-sm">
-            <div className="card-body">
+            <div className="card-body text-center">
               <h4>Project Description</h4>
-              <p>{project.long_description}</p>
+              <p className="text-center">{project.long_description}</p>
             </div>
           </div>
         </div>
         <div className="col-lg-12">
           <div className="card mb-4 shadow-sm">
-            <div className="card-body">
+            <div className="card-body text-center">
               <h4>Technologies Used</h4>
-              <p>{project.technologies}</p>
+              <p className="text-center">{project.technologies}</p>
             </div>
           </div>
         </div>
         <div className="col-lg-12">
           <div className="card shadow-sm">
-            <div className="card-body">
+            <div className="card-body text-center">
               <h4>My Contributions</h4>
-              <p>{project.contributions}</p>
+              <p className="text-center">{project.contributions}</p>
             </div>
           </div>
         </div>
